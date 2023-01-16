589fa76d21cd7add697d540d3b07b36e646ca73a
Divin Dass <116546151+WebX-Divin@users.noreply.github.com>
2023-01-16 09:54:22 +0530

    #Fix 2
    Signed-off-by: Divin Dass <116546151+WebX-Divin@users.noreply.github.com>

--- config.py
+++ config.py
@@ -2,22 +2,22 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "17318541")
+API_ID = os.environ.get("API_ID", "")
 
-API_HASH = os.environ.get("API_HASH", "3f0136ab75eaa468e7b5f3020be17588")
+API_HASH = os.environ.get("API_HASH", "")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5609721310:AAEo034QWlWz50WEbwIk-S53tplMOFe-VPw") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
 
 FORCE_SUB = os.environ.get("FORCE_SUB", "-1001860403687") 
 
-DB_NAME = os.environ.get("DB_NAME","Cluster0")     
+DB_NAME = os.environ.get("DB_NAME","")     
 
-DB_URL = os.environ.get("DB_URL","mongodb+srv://leecher-x:#DiViN143#@cluster0.e9vvmrg.mongodb.net/?retryWrites=true&w=majority")
+DB_URL = os.environ.get("DB_URL","")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
 START_PIC = os.environ.get("START_PIC", "https://graph.org/Rename-Bot-01-15")
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5328012377').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
 
 PORT = os.environ.get("PORT", "8080")
