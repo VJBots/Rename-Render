af39151d707490db17a8e8a4885aea6bb37cf622
VJBots <122880036+VJBots@users.noreply.github.com>
2023-02-04 14:38:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ DB_URL = os.environ.get("DB_URL", "mongodb+srv://Demo123:Demo123@cluster0.07fqvh
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
-START_PIC = os.environ.get("START_PIC", "https://graph.org/Rename-Bot-01-15")
+START_PIC = os.environ.get("START_PIC", None)
 
 ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5606411877').split()]
 
