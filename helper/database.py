871d53cc4e61b6d736f8a982315ceec21edfe7e9
Tech VJ <122880036+VJBots@users.noreply.github.com>
2023-09-30 16:51:46 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -1,3 +1,7 @@
+# Don't Remove Credit @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
 import motor.motor_asyncio
 from config import DB_URL, DB_NAME
 
